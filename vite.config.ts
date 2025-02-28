fb09c0da8876db94b063902f0e43c1cbac03bddc
Yaindrop <yuan_dian@live.com>
2025-02-28 22:54:08 +0800

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 // vite.config.ts
+import path from 'path'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import mkcert from 'vite-plugin-mkcert'
 
 export default defineConfig({
   base: './',
   root: 'src',
-  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.join(__dirname, 'src'),
+    },
+  },
+  plugins: [mkcert(), react()],
   build: {
     outDir: '../www',
     emptyOutDir: true,
